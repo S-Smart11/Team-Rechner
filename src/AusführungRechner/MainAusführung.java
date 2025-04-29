93aee3d77276109a3e5413a707fb88a81889e464
jaddag <jaden.dagadu@drvbund.onmicrosoft.com>
2025-04-29 11:07:56 +0200

    changed Names

--- src/AusführungRechner/MainAusführung.java
+++ src/AusführungRechner/MainAusführung.java
@@ -4,6 +4,12 @@ public class MainAusführung {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Was willst du tun?");
+		System.out.println("a"
+						+ "a"
+						+ "a"
+						+ "a");
+		
 	}
 	
 }
